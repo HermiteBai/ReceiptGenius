babf5fd82f4c38f255c89aa1428ee2445ae272a4
Lanxiao Bai <lbai5@illinois.edu>
2019-05-27 17:58:39 -0500

    TASK-5: Use for loop to render receipt cards

--- app/src/Gallery.js
+++ app/src/Gallery.js
@@ -4,22 +4,25 @@ import 'antd/dist/antd.css';
 
 class Gallery extends React.Component {
 
+  createReceipts(num) {
+    var receipts = []
+    for (var i = 0; i < num; i++) {
+      receipts.push(
+        <Col span={4}>
+          <Card title="Card title" bordered={true} hoverable={true}>
+            Card content
+          </Card>
+        </Col>
+      );
+    }
+    return receipts;
+  }
+
   render() {
     return (
       <div classNmae="Gallery" style={{ background: '#FFFFFF', padding: '30px', paddingBottom: 8 }}>
         <Row gutter={16}>
-          <Col span={4}>
-            <Card title="Card title" bordered={true} hoverable={true}>
-              Card content
-            </Card>
-          </Col>
-        </Row>
-        <Row gutter={16}>
-          <Col span={4}>
-            <Card bordered={true} hoverable={true}>
-              Card content
-            </Card>
-          </Col>
+          {this.createReceipts(10)}
         </Row>
       </div>
     );
